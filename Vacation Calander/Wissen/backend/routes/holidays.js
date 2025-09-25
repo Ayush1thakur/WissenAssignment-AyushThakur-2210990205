17ee2f793c3ce80074cb6b13d828fea5291970b4
Ayush Thakur <134188820+Ayush1thakur@users.noreply.github.com>
2025-09-26 01:11:46 +0530

    Delete Vacation Calander/Wissen directory

--- Vacation Calander/Wissen/backend/routes/holidays.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getHolidays } = require("../controllers/holidayController");
-
-router.get("/:country/:year", getHolidays);
-
-module.exports = router;
