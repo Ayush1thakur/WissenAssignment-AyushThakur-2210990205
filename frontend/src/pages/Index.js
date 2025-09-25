48d8e2fff7394b3649d10354c435df40a1813b1c
Ayush Thakur <134188820+Ayush1thakur@users.noreply.github.com>
2025-09-26 03:13:11 +0530

    Add files via upload

---
+++ frontend/src/pages/Index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import VacationCalendar from '../components/VacationCalendar';
+
+const Index = () => {
+  return React.createElement(VacationCalendar);
+};
+
+export default Index;
