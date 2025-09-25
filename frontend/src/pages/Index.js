73e3a45647aeb429600c915877a521f9206c7402
Ayush Thakur <134188820+Ayush1thakur@users.noreply.github.com>
2025-09-26 03:13:33 +0530

    Delete frontend directory

--- frontend/src/pages/Index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import VacationCalendar from '../components/VacationCalendar';
-
-const Index = () => {
-  return React.createElement(VacationCalendar);
-};
-
-export default Index;
